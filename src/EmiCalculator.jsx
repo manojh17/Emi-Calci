a6f71b55019e0d68d12533f904a8eafc4a5b5d1f
ManojhR <145860619+manojh17@users.noreply.github.com>
2025-08-11 22:00:47 +0530

    Update EmiCalculator.jsx

--- src/EmiCalculator.jsx
+++ src/EmiCalculator.jsx
@@ -3,6 +3,16 @@ import EmiForm from "./EmiForm";
 import EmiResults from "./EmiResults";
 import EmiChart from "./EmiChart";
 
+
+
+
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+
+
+
+
+
 export default function EmiCalculator() {
   const [principal, setPrincipal] = useState(0);
   const [rate, setRate] = useState(0);
@@ -38,6 +48,33 @@ export default function EmiCalculator() {
     setTotalInterest(0);
   }
 
+  function handleDownloadPDF() {
+  const pdf = new jsPDF("p", "mm", "a4");
+
+  // Title
+  pdf.setFontSize(20);
+  pdf.text("EMI Report", 105, 15, { align: "center" });
+
+  // EMI Details Table
+  pdf.setFontSize(12);
+  pdf.text(`Principal Amount: Rs.  ${principal.toLocaleString()}`, 20, 40);
+  pdf.text(`Interest Rate: ${rate}%`, 20, 50);
+  pdf.text(`Tenure: ${time} months`, 20, 60);
+  pdf.text(`Monthly EMI: Rs. ${emi.toFixed(2)}`, 20, 70);
+  pdf.text(`Total Payment: Rs. ${totalPayment.toFixed(2)}`, 20, 80);
+  pdf.text(`Total Interest: Rs. ${totalInterest.toFixed(2)}`, 20, 90);
+  pdf.text(`Generated on: ${new Date().toLocaleString()}`, 20, 210);
+
+    // Save
+  pdf.save("EMI_Report.pdf");
+ 
+
+    // Footer
+    pdf.setFontSize(10);
+
+  
+}
+
   return (
     <div className="container" style={{ maxWidth: "900px", margin: "30px auto", display: "flex", gap: "30px", color: "#fff" }}>
       <EmiForm
@@ -54,7 +91,23 @@ export default function EmiCalculator() {
       <div>
         <EmiResults emi={emi} totalPayment={totalPayment} totalInterest={totalInterest} />
         <EmiChart principal={principal} totalInterest={totalInterest} />
+        <button
+  onClick={handleDownloadPDF}
+  style={{
+    backgroundColor: "#28a745",
+    color: "white",
+    padding: "7px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+    marginTop: "10px"
+  }}
+>
+  Download EMI Report
+</button>
       </div>
+
+      
     </div>
   );
 }
